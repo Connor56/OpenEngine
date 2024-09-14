b7727addf6f9f9d379fc97551cea2bbbeea5208e
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-09-14 19:05:24 +0100

    feat: add asynchronous crawler that uses queues

--- crawler.py
+++ crawler.py
@@ -1,3 +1,12 @@
+import httpx
+import asyncio
+from typing import Dict, Any, Callable, Optional
+from bs4 import BeautifulSoup
+from typing import List
+import re
+from urllib.parse import urlparse, urlunparse
+
+
 class AsyncList:
     """
     Simple asyncronous list class that allows for thread safe
@@ -43,5 +52,101 @@ def pattern_filter(
 
     print("these are the filtered urls:", filtered_urls)
     return filtered_urls
+
+
+async def crawler(
+    url_queue: asyncio.Queue,
+    url_filter: Dict[str, Any],
+    client: httpx.AsyncClient,
+    response_queue: asyncio.Queue,
+    pause: asyncio.Event,
+    end: asyncio.Event,
+    max_iter: Optional[int] = -1,
+    seen_urls: Optional[AsyncList] = [],
+) -> None:
+    """
+    Simple asynchronous crawling function that continuously reads
+    urls from the url queue and adds the responses to a response
+    queue that can be consumed by a response processor.
+
+    Parameters
+    ----------
+    url_queue : asyncio.Queue
+        The queue that contains the urls to crawl.
+
+    url_filter : Dict[str, Any]
+        A dictionary that must contain a "filter_func" key that
+        is a callable function and a "kwargs" key that is a dictionary
+        of keyword arguments to pass to the filter function.
+
+    client : httpx.AsyncClient
+        The httpx client to use for making requests.
+
+    response_queue : asyncio.Queue
+        The queue to push responses from the request to.
+
+    pause : asyncio.Event
+        The event to pause the crawler's while loop. This lets you
+        stop and start the crawler.
+
+    end : asyncio.Event
+        Ends the crawler's while loop.
+
+    max_iter : int, optional
+        The maximum number of iterations to run the crawler for.
+
+    seen_urls : AsyncList, optional
+        The list of urls that have already been crawled. This is
+        used to prevent crawling the same url multiple times. It can
+        be shared across multiple crawlers.
+    """
+    num_iter = 0
+    while not pause.is_set():
+        # Crawler ended?
+        if end.is_set():
+            break
+
+        # Max crawl iterations reached?
+        if max_iter != -1 and num_iter >= max_iter:
+            break
+        else:
+            num_iter += 1
+
+        url = await url_queue.get()
+        url_queue.task_done()
+
+        soup = None
+        response = await client.get(url)
+
+        # Get soup object on success
+        if response.status_code == 200:
+            html = response.text
+            soup = BeautifulSoup(html, "lxml")
+
+            response_queue.put_nowait(soup)
+
+        # Get all links from the soup
+        if soup is not None:
+            all_links = soup.find_all("a")
+            all_links = [link.get("href") for link in all_links]
+
+        # Filter the links
+        filter_func = url_filter["filter_func"]
+        filter_kwargs = url_filter["kwargs"]
+
+        addable_urls = filter_func(all_links, **filter_kwargs)
+
+        # Retrieve seen urls
+        if isinstance(seen_urls, AsyncList):
+            all_urls = await seen_urls.get_all()
+        else:
+            all_urls = seen_urls[:]
+
+        # Add unseen urls to crawl queue
+        for url in addable_urls:
+            # Check it hasn't already been seen
+            if url not in all_urls:
+                await url_queue.put(url)
+                seen_urls.append(url)
 
 
