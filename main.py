44ad47937614fc9901e6b761a838c2caf20db423
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-09-20 19:19:34 +0100

    feat: main function for running crawling process

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ Description:
 Created:
     2024-09-19
 """
+
+import crawler
+
+
+def main():
+    """
+    Sets up the queues for the crawler and processor and starts the
+    index building/ updating process.
+    """
