7ae6173b84740f557c08c9354e40643703909ca9
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-09-21 11:29:19 +0100

    feat: add an asynchronous constantly running processor function
    Also refactor the module to put more important code at the top.

--- process.py
+++ process.py
@@ -21,25 +21,28 @@ class Response:
     soup: BeautifulSoup
     url: str
 
-def extract_visible_text(soup):
-    # Remove elements that do not contain user-visible text
-    for element in soup(
-        ["script", "style", "meta", "header", "footer", "nav", "noscript"]
-    ):
-        element.decompose()
 
-    # Extract the raw text
-    raw_text = soup.get_text(separator=" ")
+async def process(response_queue, model, vector_client, pause, end):
+    """
+    Processes responses collected by the crawler, turning them into
+    embeddings, and other metadata used for searching.
+    """
+    while not pause.is_set():
+        # Crawler ended?
+        if end.is_set():
+            break
+        
+        # Get response from queue
+        response: Response = await response_queue.get()
+        response_queue.task_done()
+        
+        if response.type == "webpage":
+            # Process webpage
+            soup = response.soup
+            vector, metadata = await process_html_to_vectors(soup, model, vector_client)
 
-    # Clean up the extracted text
-    visible_text = re.sub(
-        r"\s+", " ", raw_text
-    ).strip()  # Replaces multiple spaces/newlines with a single space
 
-    return visible_text
-
-
-def process_html_to_vectors(
+async def process_html_to_vectors(
     soup: BeautifulSoup,
     model: sentence_transformers.SentenceTransformer,
     db_client: QdrantClient,
@@ -69,20 +72,24 @@ def process_html_to_vectors(
     vectors = model.encode(sequences, convert_to_numpy=True)
     vectors = vectors.astype(np.float32)
 
-    # Create the collection
-    if not db_client.collection_exists("my_collection"):
-        db_client.create_collection(
-            collection_name="my_collection",
-            vectors_config=VectorParams(size=384, distance=Distance.COSINE),
-        )
+    metadata = {}
 
-    # Add the vectors to the collection
-    db_client.upsert(
-        collection_name="my_collection",
-        points=[
-            PointStruct(
-                id=idx, vector=vector.tolist(), payload={"text": sequences[idx]}
-            )
-            for idx, vector in enumerate(vectors)
-        ],
-    )
+    return vectors, metadata
+
+
+def extract_visible_text(soup):
+    # Remove elements that do not contain user-visible text
+    for element in soup(
+        ["script", "style", "meta", "header", "footer", "nav", "noscript"]
+    ):
+        element.decompose()
+
+    # Extract the raw text
+    raw_text = soup.get_text(separator=" ")
+
+    # Clean up the extracted text
+    visible_text = re.sub(
+        r"\s+", " ", raw_text
+    ).strip()  # Replaces multiple spaces/newlines with a single space
+
+    return visible_text
