757c0ea711adb00dfae82600b36aac8e8f462132
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-09-21 10:17:01 +0100

    feat: initial table for tracking urls

--- init-postgres.sh
+++ init-postgres.sh
@@ -6,3 +6,15 @@ psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
     CREATE DATABASE $POSTGRES_DB;
     GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
 EOSQL
+
+# Connect to the newly created database and create the URLVisits table
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE locations (q
+        id SERIAL PRIMARY KEY,
+        url VARCHAR(2048) NOT NULL,
+        firstVisited TIMESTAMPTZ NOT NULL,
+        lastVisited TIMESTAMPTZ NOT NULL,
+        allVisits INT DEFAULT 1,
+        externalLinks TEXT[]
+    );
+EOSQL
