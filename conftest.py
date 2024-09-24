d0abb9c401dbf5bbb3c26cffd8a1c6a33348804b
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-09-24 15:10:09 +0100

    test: postgres fixture that's populated with test values

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from datetime import datetime
 
 
 @pytest.fixture(scope="function")
@@ -37,6 +38,63 @@ def empty_postgres_client():
         );"""
 
         cursor.execute(table_sql)
+
+        yield client
+
+        client.close()
+
+    except Exception as e:
+        print(e)
+        stop_ephemeral_postgres(temp_dir)
+
+    finally:
+        print("cleaning up")
+
+        stop_ephemeral_postgres(temp_dir)
+
+
+@pytest.fixture(scope="function")
+def populated_postgres_client():
+    """
+    A fixture that provides a psycopg2 client for testing and a the function for killing
+    the server once the user is complete.
+    """
+    import psycopg2
+    from setup_postgres import start_ephemeral_postgres, stop_ephemeral_postgres
+
+    try:
+        # Start a temporary PostgreSQL server
+        print("starting postgres")
+        temp_dir, port = start_ephemeral_postgres()
+
+        # Connect to the temporary PostgreSQL server
+        client = psycopg2.connect(
+            dbname='postgres',
+            user='postgres',
+            host="localhost",
+            port=port
+        )
+        print("connected to postgres")
+
+        cursor = client.cursor()
+
+        # Create the table
+        table_sql = """CREATE TABLE resources ( 
+            id SERIAL PRIMARY KEY,
+            url VARCHAR(2048) NOT NULL,
+            firstVisited TIMESTAMPTZ NOT NULL,
+            lastVisited TIMESTAMPTZ NOT NULL,
+            allVisits INT DEFAULT 1,
+            externalLinks TEXT[]
+        );"""
+
+        cursor.execute(table_sql)
+
+        # Insert a resource
+        cursor.execute(
+            "INSERT INTO resources (url, firstVisited, lastVisited, allVisits, externalLinks) VALUES (%s, %s, %s, %s, %s)",
+            ("https://caseyhandmer.wordpress.com/", datetime.now(), datetime.now(), 1, [])
+        )
 
         yield client
 
