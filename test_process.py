36bcab19ab5a49e14bb2ff9fd16df7907f73e96e
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-09-24 21:57:42 +0100

    test: check get_base_site gets the right

--- test_process.py
+++ test_process.py
@@ -48,3 +48,12 @@ async def test_process(embedding_model, soup, vector_client, empty_postgres_clie
     assert len(results[0][5]) == 6
 
 
+def test_get_base_site():
+    """
+    Test the get_base_site function correctly extracts the base site
+    from a URL.
+    """
+    base_site = process.get_base_site("https://caseyhandmer.wordpress.com/some/random/path#an_id")
+    expected_base_site = "https://caseyhandmer.wordpress.com"
+    assert base_site == expected_base_site
+
