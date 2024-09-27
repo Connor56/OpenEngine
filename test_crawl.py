9fdb4547a1319241ccc8d3a6abfd168cbd5af7f6
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-09-27 14:41:42 +0100

    test: check crawler works when it finds an empty url in href

--- test_crawl.py
+++ test_crawl.py
@@ -99,3 +99,41 @@ async def test_crawler_seen_urls(mocker):
     assert url_queue.empty()
     assert url_queue.qsize() == 0
     assert list(url_queue._queue) == []    
+
+
+@pytest.mark.asyncio
+async def test_crawler_handles_empty_url(mocker):
+    """
+    Test the crawler function handles an empty url in an href.
+    """
+    # Set up mock response for httpx.AsyncClient.get
+    mock_response = mocker.AsyncMock()
+    mock_response.status_code = 200
+    mock_response.text = '<html><body><a href="">Example</a></body></html>'
+    mocker.patch("httpx.AsyncClient.get", return_value=mock_response)
+
+    url_queue = asyncio.Queue()
+    response_queue = asyncio.Queue()
+    pause_crawling = asyncio.Event()
+    stop_crawling = asyncio.Event()
+    await url_queue.put("https://caseyhandmer.wordpress.com/")
+
+    async with httpx.AsyncClient() as client:
+        await crawl.crawler(
+            url_queue,
+            url_filter={
+                "filter_func": crawl.pattern_filter,
+                "kwargs": {"regex_patterns": ["https://"]},
+            },
+            client=client,
+            response_queue=response_queue,
+            pause=pause_crawling,
+            end=stop_crawling,
+            max_iter=1,
+            seen_urls=[],
+        )
+
+    # Check url queue outputs
+    assert url_queue.empty()
+    assert url_queue.qsize() == 0
+    assert list(url_queue._queue) == []
