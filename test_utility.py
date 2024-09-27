d49fbd89737a4409b6b387f85a053f2aaccac71b
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-09-27 14:40:59 +0100

    refactor: black format code

--- test_utility.py
+++ test_utility.py
@@ -18,10 +18,12 @@ def test_clean_urls():
     cleaned_urls = utility.clean_urls(urls)
 
     assert len(cleaned_urls) == 2
-    assert set(cleaned_urls) == set([
-        "https://example.com",
-        "https://example.com/example",
-    ])
+    assert set(cleaned_urls) == set(
+        [
+            "https://example.com",
+            "https://example.com/example",
+        ]
+    )
 
 
 def test_handle_relative_url():
@@ -29,18 +31,28 @@ def test_handle_relative_url():
     Test the handle_relative_url function correctly handles relative
     urls.
     """
-    current_url = "https://caseyhandmer.wordpress.com/test_data/simple_site/page1.html"
+    current_url = (
+        "https://caseyhandmer.wordpress.com/test_data/simple_site/page1.html"
+    )
     base_site = "https://caseyhandmer.wordpress.com"
 
     # Test relative url
     relative_url = "/page2.html"
     expected_url = "https://caseyhandmer.wordpress.com/page2.html"
-    assert utility.handle_relative_url(relative_url, current_url, base_site)[0] == expected_url
+    assert (
+        utility.handle_relative_url(relative_url, current_url, base_site)[0]
+        == expected_url
+    )
 
     # Test relative url with query string
     relative_url = "page2.html"
-    expected_url = "https://caseyhandmer.wordpress.com/test_data/simple_site/page2.html"
-    assert utility.handle_relative_url(relative_url, current_url, base_site)[0] == expected_url
+    expected_url = (
+        "https://caseyhandmer.wordpress.com/test_data/simple_site/page2.html"
+    )
+    assert (
+        utility.handle_relative_url(relative_url, current_url, base_site)[0]
+        == expected_url
+    )
 
 
 def test_handle_relative_url():
@@ -66,6 +78,8 @@ def test_get_base_site():
     Test the get_base_site function correctly extracts the base site
     from a URL.
     """
-    base_site = utility.get_base_site("https://caseyhandmer.wordpress.com/some/random/path#an_id")
+    base_site = utility.get_base_site(
+        "https://caseyhandmer.wordpress.com/some/random/path#an_id"
+    )
     expected_base_site = "https://caseyhandmer.wordpress.com"
     assert base_site == expected_base_site
