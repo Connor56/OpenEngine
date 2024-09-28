a56e1ca248f1dff4eeab8e0520c5562d59975d55
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-09-28 23:14:55 +0100

    test: make a base qdrant client for re-use in the session

--- conftest.py
+++ conftest.py
@@ -140,21 +140,34 @@ def populated_postgres_client(base_postgres_client):
     cursor.execute("DROP TABLE resources")
 
 
-@pytest.fixture(scope="function")
-def vector_client():
+@pytest.fixture(scope="session")
+def base_vector_client():
     """
-    A fixture that provides a Qdrant client with the embeddings collection
-    for testing.
+    A fixture that provides a base qdrant client in memory that's used
+    throughout the tests.
     """
     from qdrant_client import QdrantClient
-    from qdrant_client.models import VectorParams, Distance
 
     client = QdrantClient(":memory:")
-    client.create_collection(
+    return client
+
+
+@pytest.fixture(scope="function")
+def vector_client(base_vector_client):
+    """
+    A fixture that provides a Qdrant client with the embeddings collection
+    for testing. That is cleaned once the function is complete.
+    """
+    from qdrant_client.models import VectorParams, Distance
+
+    base_vector_client.create_collection(
         collection_name="embeddings",
         vectors_config=VectorParams(size=384, distance=Distance.COSINE),
     )
-    return client
+
+    yield base_vector_client
+
+    base_vector_client.delete_collection("embeddings")
 
 
 @pytest.fixture(scope="session")
