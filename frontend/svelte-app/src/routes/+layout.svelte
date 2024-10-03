65b6f9cc6bb35923502f8bc174396e8bb9358268
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-10-04 00:47:13 +0100

    feat: set global font styles for the application

---
+++ frontend/svelte-app/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<link
+	href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+	rel="stylesheet"
+/>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	:global(body) {
+		font-family: 'Roboto', sans-serif;
+	}
+</style>
