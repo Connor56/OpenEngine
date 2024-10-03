f21cbddc4287411b90049cc513878ffb2ad49ce8
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-10-04 00:44:55 +0100

    feat: add SearchBar component for initial and results page

---
+++ frontend/svelte-app/src/lib/components/SearchBar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let handleKeyPress: (event: KeyboardEvent) => void;
+
+	let query = '';
+</script>
+
+<div class="search-bar">
+	<input
+		type="text"
+		class="search-input"
+		bind:value={query}
+		on:keypress={handleKeyPress}
+		placeholder="Search..."
+	/>
+</div>
+
+<style>
+	.search-input {
+		width: 550px;
+		height: 24px;
+		padding: 10px 20px;
+		font-size: 18px;
+		font-family: 'Roboto', sans-serif;
+		border-radius: 100px;
+		border: 1px solid #dfe1e5;
+	}
+
+	.search-input:focus {
+		outline: none;
+		border: 1px solid rgb(255, 255, 255);
+		box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+	}
+
+	.search-input:hover {
+		outline: none;
+		border: 1px solid #ffffff;
+		box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+	}
+</style>
