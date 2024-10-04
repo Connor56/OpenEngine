8f8cd9661aa1ee685c1373090cda8306fcab8f2f
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-10-04 12:43:56 +0100

    feat: add standard button for site

---
+++ frontend/svelte-app/src/lib/components/FormButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let label: string;
+</script>
+
+<button type="submit">{label}</button>
+
+<style>
+	button {
+		width: 100%;
+		padding: 10px;
+		font-size: 16px;
+		border-radius: 10px;
+		border: 1px solid #dfe1e5;
+		background-color: #ffffff;
+		color: #368aff;
+		cursor: pointer;
+	}
+
+	button:hover {
+		background-color: #368aff;
+		color: #ffffff;
+		box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+	}
+</style>
