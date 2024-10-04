ac249726d638bd3a0c8548fe391be52ae9c7fb66
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-10-04 13:49:04 +0100

    feat: add the AdminTopNav to the admin page

--- frontend/svelte-app/src/routes/admin/+page.svelte
+++ frontend/svelte-app/src/routes/admin/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+	import AdminTopNav from '$lib/components/AdminTopNav.svelte';
 
 	let seedUrls = ['https://an_url.com'];
 	let newUrl = '';
 </script>
 
+<AdminTopNav />
+<div class="admin-container"></div>
 
 <style>
 	/* Your styles here */
@@ -31,5 +34,10 @@
 	.add-url input {
 		flex: 1;
 		margin-right: 10px;
+	}
+
+	:global(body) {
+		background-color: #ffffff;
+		margin: 0;
 	}
 </style>
