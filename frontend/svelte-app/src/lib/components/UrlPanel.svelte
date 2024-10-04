2a2f3ada76b2e7d9095896bfbbe862f4066af49b
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-10-04 13:47:29 +0100

    feat: add url panel for containing seed urls

---
+++ frontend/svelte-app/src/lib/components/UrlPanel.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import type { Link } from '$lib/types';
+
+	export let links: Link[];
+</script>
+
+<div class="link-panel">
+	{#each links as link}
+		<div class="link-item">
+			<a class="link-title" href={link.url}>{link.title}</a>
+			<div class="link-url">{link.url}</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.link-panel {
+		margin-top: 30px;
+	}
+
+	.link-item {
+		margin-bottom: 20px;
+	}
+
+	.link-title {
+		font-size: 18px;
+		color: #368aff;
+		text-decoration: none;
+	}
+
+	h3 {
+		font-weight: 400;
+		margin-top: 8px;
+		margin-bottom: 8px;
+	}
+
+	.link-title:hover {
+		text-decoration: underline;
+	}
+
+	.link-title:visited {
+		color: #b45eff;
+	}
+
+	.link-url {
+		font-size: 14px;
+		color: #191919;
+		margin-top: 0;
+	}
+</style>
