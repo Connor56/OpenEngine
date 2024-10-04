681d663d648d7cedeab439550a761a799c8f2dd7
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-10-05 00:07:31 +0100

    refactor: remove unused styles

--- frontend/svelte-app/src/lib/components/UrlPanel.svelte
+++ frontend/svelte-app/src/lib/components/UrlPanel.svelte
@@ -16,33 +16,11 @@
 		margin-top: 30px;
 	}
 
-	.url-item {
-		margin-bottom: 20px;
 	}
 
-	.url-title {
-		font-size: 18px;
-		color: #368aff;
-		text-decoration: none;
 	}
 
-	h3 {
-		font-weight: 400;
-		margin-top: 8px;
-		margin-bottom: 8px;
 	}
 
-	.url-title:hover {
-		text-decoration: underline;
-	}
-
-	.url-title:visited {
-		color: #b45eff;
-	}
-
-	.url-url {
-		font-size: 14px;
-		color: #191919;
-		margin-top: 0;
 	}
 </style>
