c9f84d09ae54292e8c929098aae0b4aa55fdf31e
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-10-07 00:04:02 +0100

    feat: set a lifespan for the app that sets up postgres and qdrant clients on startup

--- app/main.py
+++ app/main.py
@@ -6,19 +6,55 @@ Created:
     2024-09-29
 """
 
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 import jwt
 from typing import Optional
+from dotenv import load_dotenv
+import asyncpg
+from qdrant_client import AsyncQdrantClient
+import os
 
-app = FastAPI()
+load_dotenv(dotenv_path="../.env")
 
 # OAuth2 scheme setup (even though we're just using JWT, it's needed here)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+# Global database clients
+postgres_client = None
+qdrant_client = None
 
 @app.post("/login")
 async def admin_login():
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Lifespan sets up the database clients for the app. On
+    shutdown the clients are closed.
+    """
+    global postgres_client
+    global qdrant_client
+
+    # Set up the database clients
+    postgres_client = await asyncpg.connect(
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        host=os.getenv("POSTGRES_HOST"),
+        port=os.getenv("POSTGRES_PORT"),
+    )
+
+    # set up the qdrant client
+    qdrant_client = AsyncQdrantClient(
+        host=os.getenv("QDRANT_URL"),
+        port=os.getenv("QDRANT_PORT"),
+    )
+
+
+# Set up the FastAPI app with lifespan
+app = FastAPI(lifespan=lifespan)
     """
     Logs in an admin user by checking their credentials.
     """
