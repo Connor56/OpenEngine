dfb60ee534cf146debe10dd60533a335deb408ea
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2024-10-07 00:48:37 +0100

    refactor: drop placeholder endpoints

--- app/main.py
+++ app/main.py
@@ -100,26 +100,3 @@ async def admin_login(
     access_token = auth.create_access_token(data={})
 
     return {"token": access_token, "type": "bearer"}
-
-
-# Dependency to extract and validate the JWT
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-    except jwt.JWTError:
-        raise credentials_exception
-    return username
-
-
-# Protected route
-@app.get("/hello")
-async def read_hello(current_user: str = Depends(get_current_user)):
-    return {"message": f"Hello, {current_user}!"}
